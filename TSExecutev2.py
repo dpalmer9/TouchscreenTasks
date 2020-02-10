a7634e22d89357ce6b582c14c2262ae082140496
dpalmer9 <dpalmer9@uwo.ca>
2020-02-10 12:36:56 -0500

    Added New CPT Development Protocol

--- TSExecutev2.py
+++ TSExecutev2.py
@@ -27,7 +27,7 @@ class Touchscreen_UI():
 
         #self.protocol_list = [protocol.replace('.py','') for protocol in self.protocol_list]
 
-        self.protocol_list = ['iCPT','iCPTImage2','iCPTStimDurationScreen','iCPTImageScreen','vPRL','PAL','TUNL','PR']
+        self.protocol_list = ['iCPT','iCPTImage2','iCPTStimDurationScreen','iCPTImageScreen','iCPTStimImageScreen','vPRL','PAL','TUNL','PR']
 
         self.main_screen = tk.Tk()
 
